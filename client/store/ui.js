5dc6280f05a6fbd11f875b9e6991d7cc8bc78e61
Gahan Rakholia <gahan.rakholia@jda.com>
2018-03-18 17:54:35 +0530

    autoupdate with button added

--- client/store/ui.js
+++ client/store/ui.js
@@ -1,7 +1,13 @@
+export const UPDATE_TRACKER_STEP = 'UPDATE_TRACKER_STEP'
+
 const state = {
+  currentTrackerStep: 0
 }
 
 const mutations = {
+  [UPDATE_TRACKER_STEP] (state, payload){
+    state.currentTrackerStep = payload
+  }
 }
 
 export default {
--- client/views/Tracker.vue
+++ client/views/Tracker.vue
@@ -2,17 +2,17 @@
   <div>
     <v-layout row>
       <v-flex xs6 offset-xs3>
-        <v-stepper alt-labels v-model="currentStep">
+        <v-stepper alt-labels v-model="currentTrackerStep">
           <v-stepper-header>
-            <v-stepper-step step="1" :complete="currentStep > 1" editable edit-icon="shopping_cart"> Created </v-stepper-step>
+            <v-stepper-step step="1" :complete="currentTrackerStep > 1" editable edit-icon="shopping_cart"> Created </v-stepper-step>
             <v-divider />
-            <v-stepper-step step="2" :complete="currentStep > 2" editable edit-icon="assignment"> Planning </v-stepper-step>
+            <v-stepper-step step="2" :complete="currentTrackerStep > 2" editable edit-icon="assignment"> Planning </v-stepper-step>
             <v-divider />
-            <v-stepper-step step="3" :complete="currentStep > 3" editable edit-icon="assessment"> Planned </v-stepper-step>
+            <v-stepper-step step="3" :complete="currentTrackerStep > 3" editable edit-icon="assessment"> Planned </v-stepper-step>
             <v-divider />
-            <v-stepper-step step="4" :complete="currentStep > 4" editable edit-icon="flight_takeoff"> In-Transit </v-stepper-step>
+            <v-stepper-step step="4" :complete="currentTrackerStep > 4" editable edit-icon="flight_takeoff"> In-Transit </v-stepper-step>
             <v-divider />
-            <v-stepper-step step="5" :complete="currentStep > 5" editable> Arrived </v-stepper-step>
+            <v-stepper-step step="5" :complete="currentTrackerStep > 5" editable> Arrived </v-stepper-step>
           </v-stepper-header>
         </v-stepper>
       </v-flex>
@@ -22,12 +22,22 @@
         <status-detail :order="order" ></status-detail>
       </v-flex>
     </v-layout>
+    <v-btn @click="checkOrderStatus($route.params.orderId)" >Update Order</v-btn>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapMutations } from 'vuex'
 import StatusDetail from '../components/StatusDetail.vue'
+import { UPDATE_TRACKER_STEP } from '../store/ui'
+import {
+  STATUS_CREATED,
+  STATUS_PLANNING,
+  STATUS_PLANNED,
+  STATUS_IN_TRANSIT,
+  STATUS_ARRIVED,
+  STATUS_CANCELED
+} from '../store/order'
 
 export default {
   data: function() {
@@ -37,18 +47,79 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['fetchOrderById'])
+    ...mapActions(['fetchOrderById']),
+    ...mapMutations([`${UPDATE_TRACKER_STEP}`]),
+    checkOrderStatus: function(orderId) {
+      let prevOrderState = JSON.parse(JSON.stringify(this.order))
+      this.fetchOrderById(this.$route.params.orderId).then(res => {
+        let newOrderState = JSON.parse(JSON.stringify(res.data))
+        if (newOrderState.orderStatus != prevOrderState.orderStatus) {
+          let newStep = -1
+          switch (newOrderState.orderStatus) {
+            case STATUS_CREATED:
+              newStep = 1
+              break
+            case STATUS_PLANNING:
+              newStep = 2
+              break
+            case STATUS_PLANNED:
+              newStep = 3
+              break
+            case STATUS_IN_TRANSIT:
+              newStep = 4
+              break
+            case STATUS_ARRIVED:
+              newStep = 5
+              break
+            case STATUS_CANCELED:
+              newStep = 6
+          }
+          this.UPDATE_TRACKER_STEP(newStep)
+        }
+      }) // fetch new order
+    }
   },
-  beforeRouteUpdate: function(to, from, next){
+  beforeRouteUpdate: function(to, from, next) {
     this.fetchOrderById(to.params.orderId)
+    this.UPDATE_TRACKER_STEP(1) //reset tracker on change of order
     next()
   },
   mounted: function() {
-    this.fetchOrderById(this.$route.params.orderId)
+    this.fetchOrderById(this.$route.params.orderId).then(res => {
+      let newStep = -1
+      switch (res.data.orderStatus) {
+        case STATUS_CREATED:
+          newStep = 1
+          break
+        case STATUS_PLANNING:
+          newStep = 2
+          break
+        case STATUS_PLANNED:
+          newStep = 3
+          break
+        case STATUS_IN_TRANSIT:
+          newStep = 4
+          break
+        case STATUS_ARRIVED:
+          newStep = 5
+          break
+        case STATUS_CANCELED:
+          newStep = 6
+      }
+      this.UPDATE_TRACKER_STEP(newStep)
+    })
   },
   computed: {
     order: function() {
       return this.$store.getters.getOrder
+    },
+    currentTrackerStep: {
+      get() {
+        return this.$store.state.ui.currentTrackerStep
+      },
+      set(step) {
+        return this.UPDATE_TRACKER_STEP(step)
+      }
     }
   },
   components: {
